18b6ca92fe786bc1613fa6655c7aa1c6db6a5907
Alexey Savchenko <wasxela@gmail.com>
2020-12-01 20:02:13 +0200

    have done task1 task2 from day_2

---
+++ day_2/main.js
@@ -0,0 +1,15 @@
+import { howMuchOneInNumber } from './src/task1.js';
+import { sortWords } from './src/task2.js';
+import {} from './src/task3.js';
+
+console.log('task 1 => ', howMuchOneInNumber('123456'));
+console.log('task 1 => ', howMuchOneInNumber(- 123456));
+console.log('task 1 => ', howMuchOneInNumber(123467));
+
+
+console.log('task 2 => ', sortWords('is2      Thi1s T4est 3a'));
+console.log('task 2 => ', sortWords('4of Fo1r pe6ople g3ood th5e the2'));
+console.log('task 2 => ', sortWords(''));
+
+
+
