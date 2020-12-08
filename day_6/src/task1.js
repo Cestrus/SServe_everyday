822dc3de74810a97b95d69416cca21f5385d872d
Alexey Savchenko <wasxela@gmail.com>
2020-12-08 10:52:52 +0200

    doing task day_6 #2

--- day_6/src/task1.js
+++ day_6/src/task1.js
@@ -1,23 +1,27 @@
 
 export const countIP = (ipStart, ipFinish) => {
   const arrIP = [[...ipStart.split('.')], [...ipFinish.split('.')]] ;
-  // arrIP.sort();
+  arrIP.sort();
 
-  let sum = 0;
-
+  let buffer = new ArrayBuffer(4);
+  let arrBits = new Uint8Array(buffer);
+  let strBits = '';
+  
   for (let i = 0; i < 4; i++){
-    // if (!isValidIP(arrIP[i])) {
-    //   console.log('invalid IP');
-    //   break;
-    // }
-    sum += (arrIP[0][i] > arrIP[1][i])? arrIP[0][i] - arrIP[1][i] : arrIP[1][i] - arrIP[0][i];
-
-
+    arrBits[i] = Number(arrIP[1][i]);
+    arrBits[i] = arrBits[i] - Number(arrIP[0][i]);
   }
   
+  for (let i = 0; i < arrBits.length; i++){
+    let num = arrBits[i];
+    strBits += num.toString(2);
+  }
 
   
-  console.log(sum);
+  console.log(arrBits);
+  console.log(strBits, +strBits.toString(2));
+
+  return strBits.toString(10)
 }
 
 
