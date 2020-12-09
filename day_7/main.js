cf58507f9595098246bef56f3241186629230a8b
Alexey Savchenko <wasxela@gmail.com>
2020-12-09 22:32:18 +0200

    have done task2 day_7

--- day_7/main.js
+++ day_7/main.js
@@ -5,7 +5,7 @@ const arrMock_1 = [[0, 0, 1], [0, 1, 2], [2, 1, 0]];
 const arrMock_2 = [[0, 1, 1], [0, 1, 2], [2, 1, 0]];
 const arrMock_3 = [[2, 1, 1], [0, 2, 2], [0, 1, 2]];
 
-const text = 'Yesterday, we bumped into Laura. It had to happen, but you can\'t deny the timing couldn\'t be worse. The "mission" to try and seduce her was a complete failure last month. By the way, she still has the ring I gave her. Anyhow, it hasn\'t been a pleasurable experience to go through it. I wanted to feel done with it first.'
+const text = 'Yesterday, we bumped into Laura. It had to happen, but you can\'t deny the timing couldn\'t be worse. The "mission" to try and seduce her was a complete failure last month. By the way, she still has the ring I gave her. Anyhow, it hasn\'t been a pleasurable experience to go through it. I wanted to feel done with it first.';
 
 // console.log('task 1 => ', checkDesk(arrMock_1));
 // console.log('task 1 => ', checkDesk(arrMock_2));
--- day_7/src/task2.js
+++ day_7/src/task2.js
@@ -4,7 +4,6 @@ export const decodeText = (text) => {
   let keys = [];
   let decText = '';
 
-  // 
   for (let i = 0, j = 0; i < arrString.length; i++){
     if (i === 0 || i === keys.length + 1){
       keys.length = 0;
@@ -12,11 +11,11 @@ export const decodeText = (text) => {
       continue;
     } else {
       let arrWords = arrString[i].split(' ');
-      decText += arrWords[keys[j]];
+      decText += (deleteSign(arrWords[keys[j]]) + ' ');
       j++;
     }    
   }
-  return decText;
+  return firstLetterAndDota(decText.trim());
 }
 
 const mySplitText = (text) => {
@@ -35,30 +34,33 @@ const mySplitText = (text) => {
 }
 
 const extractKeys = (string) => {
-  const keys = [];
-  const arrWords = deleteSign( string.split(' ') );
+  let keys = [];
+  const arrWords = string.split(' ');
 
   for (let i = 0; i < arrWords.length; i++){
-    keys.push(arrWords[i].length)
+    let word = deleteSign(arrWords[i]);
+    keys.push(word.length)
   }
   return keys;
 }
 
-const deleteSign = (arrWords) => {
-  const forbSign = '.,\'"`!?;:';
-  const wordsNotSign = [];
+const deleteSign = (word) => {
+  const arrLetter = [...word];
+  const forbSign = ".,\"`!?;:'";
+  let wordNotSign = '';
 
-  for (let i = 0; i < arrWords.length; i++){
-    let arrLetter = [...arrWords[i]];
-    let word = '';
-
-    for (let j = 0; j < arrLetter.length; j++){      
-      for (let k = 0; k < forbSign.length; k++){
-        if (arrLetter[j] !== forbSign[k]) word += arrLetter[j];
-      }
+  for (let i = 0; i < arrLetter.length; i++){      
+    for (let j = 0; j < forbSign.length; j++){
+      if (arrLetter[i] === forbSign[j]) break;
+      else if (j === forbSign.length - 1) wordNotSign += arrLetter[i];
     }
-    wordsNotSign.push(word);
-  } 
-  return wordsNotSign;
+  }
+  return wordNotSign;
 }
 
+const firstLetterAndDota = (string) => {
+  const arr = [...string];
+  arr[0] = arr[0].toUpperCase();
+  arr[arr.length] = '.';
+  return arr.join('');
+}
