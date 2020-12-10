8935f31cd33f238ebbb209ee1c2abf38a3a3d0a0
Alexey Savchenko <wasxela@gmail.com>
2020-12-11 00:06:32 +0200

    doing task2 day_8

---
+++ day_8/src/task2.js
@@ -0,0 +1,12 @@
+
+const damagedOrSunk = (board, attacks) => {
+  const myBoard = board;
+
+
+
+
+}
+
+const countAttackPoint = (board, attack) => {
+  return board[board.length - attack[1]][attack[0] - 1]
+}
